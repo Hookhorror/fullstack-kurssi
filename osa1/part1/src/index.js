857693074cb2e0e251792d58c9b1b3f0e8c3fef3
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2019-09-28 17:24:08 +0300

    osa 1
    aloitettu kurssitiedot tehtävän tekeminen

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
 const App = () => {
+
+  const nimi  = 'Pekka'
+  const ika   = 10 
+
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
+      <Hello name="Maya" age={26+10} />
+      <Hello name={nimi} age={ika} />      
+    </>
   )
 }
 
