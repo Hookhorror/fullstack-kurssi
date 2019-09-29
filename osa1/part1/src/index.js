a505a5835efc0f871f7eb6047986525ea6354ff0
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2019-09-29 17:55:36 +0300

    osa1
    osa 1c käyty läpi

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -1,30 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = ({ name, age }) => {
+const Display = ({ counter }) => <div>{counter}</div>
+  const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
 
-  const bornYear = () => new Date().getFullYear() - age
+const App = (props) => {
+
+
+  
+
+
+  const [ counter, setCounter ] = useState(0)
+
+  const setToValue = (value) => setCounter(value)
 
   return (
     <div>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born {bornYear()}</p>
+      <Display counter={counter}/>
+      <Button
+        onClick={() => setToValue(counter + 1)}
+        text='plus'
+        
+      />
+      <Button
+        onClick={() => setToValue(counter - 1)}
+        text='minus'
+      />
+      <Button
+        onClick={() => setToValue(0)}
+        text='zero'
+      />
     </div>
   )
 }
 
-const App = () => {
-
-  const nimi  = 'Pekka'
-  const ika   = 10 
-
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Arto" age={26+10} />
-      <Hello name={nimi} age={ika} />      
-    </>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
