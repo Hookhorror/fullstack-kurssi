d8992fda9f1962d43a0fa46d949525b4e2316eec
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2019-10-04 17:09:35 +0300

    osa1-d
    kohta ennen "taulukon käsittelyä"

--- osa1/osa1-d/src/index.js
+++ osa1/osa1-d/src/index.js
@@ -7,21 +7,11 @@ const App = (props) => {
     left: 0, right: 0
   })
 
-  const handleLeftClick = () => {
-    const newClicks = { 
-      ...clicks, 
-      left: clicks.left + 1 
-    }
-    setClicks(newClicks)
-  }
-  
-  const handleRightClick = () => {
-    const newClicks = { 
-      ...clicks, 
-      right: clicks.right + 1 
-    }
-    setClicks(newClicks)
-  }
+  const handleLeftClick = () =>
+    setClicks({ ...clicks, left: clicks.left + 1 })
+
+  const handleRightClick = () =>
+    setClicks({ ...clicks, right: clicks.right + 1 })
 
   return (
     <div>
